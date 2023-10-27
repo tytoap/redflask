93601510c6d11cbff2265ff2b5e7218cee6ed0c8
tytoap <37224788+tytoap@users.noreply.github.com>
2023-10-27 15:01:18 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask, make_response, jsonify, redirect, render_template, request, url_for
+ 
+ 
+  
+ 
+
+app = Flask(__name__)
+
+ 
+ 
+
+
+
+ 
+@app.route("/")
+def hello_world():
+    
+    
+   return "Hello"
+ 
+         
+  
+
+        
+
+
+        
+ 
+
+if __name__ == '__main__':
+ 
+  app.run('localhost', 8080, debug=True)
